3b4192e5bcc015822c9fdb83a7e8b11b51ebb869
crisoberyl@naver.com <crisoberyl@naver.com>
2019-04-12 17:26:14 +0900

    1

---
+++ src/main/java/com/osf/sp/ioc2/Kia.java
@@ -0,0 +1,12 @@
+package com.osf.sp.ioc2;
+
+public class Kia implements CarMaker {
+	private Car c;
+	public Kia(Car c) {
+		this.c = c;
+	}
+	
+	public void sellCar() {
+		System.out.println("기아 자동차 : " + c + "를 판다");
+	}
+}
